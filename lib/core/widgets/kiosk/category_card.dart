e8a884d47b758df13adbc178bd7b6562c2d7b6a2
unknown <rlagustn070@naver.com>
2025-05-02 17:05:12 +0900

    feat : category card 컴포넌트 생성

---
+++ lib/core/widgets/kiosk/category_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:team_kiosk/core/constants/app_colors.dart';
+import 'package:team_kiosk/core/constants/box_styles.dart';
+import 'package:team_kiosk/core/constants/theme_provider.dart';
+
+enum Category { burger, cafe }
+
+class CategoryCard extends ConsumerWidget {
+  final Category category;
+  final KioskTheme theme;
+  final String text;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  CategoryCard({
+    super.key,
+    required this.icon,
+    required this.category,
+    required this.theme,
+    required this.text,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final styles = ref.watch(textStyleSetProvider);
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: onTap,
+          style: ButtonStyles.categoryButton(theme.primary),
+          child: FittedBox(
+            child: Row(
+              children: [
+                Icon(icon, color: Colors.white, size: 30),
+                const SizedBox(width: 5),
+                Text(text, style: styles.button),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
