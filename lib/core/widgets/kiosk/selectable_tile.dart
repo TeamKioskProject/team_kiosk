9c208f6b1e27c3e857a6d5603caf5d364eb79b4e
unknown <rlagustn070@naver.com>
2025-05-02 17:50:08 +0900

    feat : selectable_tile 컴포넌트 추가

---
+++ lib/core/widgets/kiosk/selectable_tile.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:team_kiosk/core/constants/app_colors.dart';
+import 'package:team_kiosk/core/constants/box_styles.dart';
+import 'package:team_kiosk/core/constants/theme_provider.dart';
+
+class SelectableTile extends ConsumerWidget {
+  final String image;
+  final String title;
+  final IconData icon;
+  final KioskTheme theme;
+  final VoidCallback onTap;
+
+  SelectableTile({
+    super.key,
+    required this.theme,
+    required this.onTap,
+    required this.image,
+    required this.title,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final styles = ref.watch(textStyleSetProvider);
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: ButtonStyles.kioskButton(Colors.white),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Image.asset(image, width: double.infinity, fit: BoxFit.cover),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Row(
+                      children: [
+                        Icon(icon, color: theme.primary, size: 28),
+                        const SizedBox(width: 3),
+                        Flexible(
+                          child: Text(
+                            title,
+                            style: styles.headline2,
+                            overflow: TextOverflow.visible,
+                            maxLines: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Icon(Icons.arrow_forward_ios),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
