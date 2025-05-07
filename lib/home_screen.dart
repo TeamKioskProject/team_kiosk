a1e488394e74263e8425d5922f549ec18153a9b6
YUNSEO <og737596@gmail.com>
2025-05-07 14:29:06 +0900

    fix : test error fix

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:team_kiosk/core/constants/theme_provider.dart';
 import 'package:team_kiosk/core/widgets/kiosk/kiosk_button.dart';
-import 'package:team_kiosk/core/widgets/payment/installment_group.dart';
 import 'package:team_kiosk/core/widgets/payment/mock_data.dart';
 import 'package:team_kiosk/core/widgets/payment/order_summary_box.dart';
 import 'package:team_kiosk/core/widgets/payment/signature_card.dart';
@@ -22,6 +21,8 @@ class HomeScreen extends ConsumerWidget {
         padding: const EdgeInsets.all(30),
         child: Column(
           children: [
+            Kioskbutton(text: '오늘은 몇 가지 주문을 연습해볼까요?', theme: theme,textStyleSet: styles,),
+            const SizedBox(height: 20),
             Column(
               children:
                   mockData.map((items) {
@@ -38,25 +39,8 @@ class HomeScreen extends ConsumerWidget {
                     );
                   }).toList(),
             ),
-            Kioskbutton(
-              text: '오늘은 몇 가지 주문을 연습해볼까요?',
-              theme: theme,
-              textStyleSet: styles,
-            ),
-
-            Column(
-              children:
-                  mockData.map((items) {
-                    return OrderSummaryBox(
-                      theme: theme,
-                      textStyleSet: styles,
-                      itemImage: items["menuImgThumPath"],
-                      itemName: items["menuNm"],
-                      itemPrice: items["price"].toString(),
-                      itemQuantity: 1,
-                    );
-                  }).toList(),
-            ),
+            TotalAmount(theme: theme, textStyleSet: styles, amount: '25800'),
+            SignatureCard(theme: theme, styles: styles),
           ],
         ),
       ),
