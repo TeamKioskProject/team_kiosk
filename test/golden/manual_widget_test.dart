09c2bdc9b362048d3089e4e8ea47efa8fc640e8b
YUNSEO <og737596@gmail.com>
2025-05-07 14:00:27 +0900

    test : test code 추가

--- test/golden/manual_widget_test.dart
+++ test/golden/manual_widget_test.dart
@@ -11,7 +11,10 @@ import 'package:team_kiosk/core/widgets/kiosk/kiosk_button.dart';
 import 'package:team_kiosk/core/widgets/kiosk/selectable_tile.dart';
 import 'package:team_kiosk/core/widgets/payment/dialog_action_button.dart';
 import 'package:team_kiosk/core/widgets/payment/dialog_cancel_button.dart';
+import 'package:team_kiosk/core/widgets/payment/installment_group.dart';
 import 'package:team_kiosk/core/widgets/payment/payment_method_tile.dart';
+import 'package:team_kiosk/core/widgets/payment/signature_card.dart';
+import 'package:team_kiosk/core/widgets/payment/total_amount.dart';
 
 void main() {
   final theme = KioskTheme.fromMode(KioskMode.cafe);
@@ -55,6 +58,9 @@ void main() {
         icon: Icons.handyman_sharp,
       ),
     ),
+    'SignatureCard': SignatureCard(styles: AppTextStyles.normal, theme: theme),
+    'TotalAmount' : TotalAmount(theme: theme, textStyleSet: AppTextStyles.normal, amount: '25800'),
+    'InstallmentGroup' : InstallmentGroup(theme: theme, textStyleSet: AppTextStyles.normal)
   };
 
   final screenSizes = [
