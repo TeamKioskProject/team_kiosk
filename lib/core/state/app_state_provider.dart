d659dad6732420839b86cd4197966f42f390cba2
unknown <rlagustn070@naver.com>
2025-05-08 10:09:09 +0900

    delete app_state_provider.dart

--- lib/core/state/app_state_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:riverpod/riverpod.dart';
-import 'package:team_kiosk/core/state/app_state.dart';
-import 'package:team_kiosk/core/state/app_state_notifier.dart';
-
-final appStateProvider = NotifierProvider<AppStateNotifier, AppState>(
-  AppStateNotifier.new,
-);
