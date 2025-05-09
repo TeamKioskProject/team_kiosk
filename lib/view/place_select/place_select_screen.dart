6bb67b5c0e428ab22c5872ff4ffec625ed7d42d0
unknown <rlagustn070@naver.com>
2025-05-09 13:30:07 +0900

    ui : place_select_screen 위젯 간격 수정

--- lib/view/place_select/place_select_screen.dart
+++ lib/view/place_select/place_select_screen.dart
@@ -38,6 +38,7 @@ class PlaceSelectScreen extends ConsumerWidget {
                         : '커피를 어디서 드시겠어요?',
                 theme: theme,
               ),
+              const SizedBox(height: 20),
               SelectableTile(
                 theme: theme,
                 icon: Icons.restaurant,
