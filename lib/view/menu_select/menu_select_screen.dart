d1edd21e2a1ce02cb22050b77d2e62df45c848b6
unknown <rlagustn070@naver.com>
2025-05-09 14:22:05 +0900

    feat : menu_select_screen에 step_progress_bar 추가

--- lib/view/menu_select/menu_select_screen.dart
+++ lib/view/menu_select/menu_select_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:team_kiosk/core/constants/app_colors.dart';
 import 'package:team_kiosk/core/constants/theme_provider.dart';
 import 'package:team_kiosk/core/widgets/kiosk/kiosk_app_bar.dart';
+import 'package:team_kiosk/core/widgets/kiosk/step_progress_bar.dart';
 
 class MenuSelectScreen extends ConsumerWidget {
   const MenuSelectScreen({super.key});
@@ -10,16 +12,45 @@ class MenuSelectScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.read(kioskThemeProvider);
     final style = ref.read(textStyleSetProvider);
-    return Scaffold(
-      appBar: KioskAppBar(
-        title: '메뉴 선택',
-        theme: theme,
-        action: [
-          const Padding(
-            padding: EdgeInsets.only(right: 16.0), // 원하는 만큼 조절
-            child: Icon(Icons.volume_up),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(110),
+          child: Column(
+            children: [
+              KioskAppBar(
+                title: '메뉴 선택',
+                theme: theme,
+                action: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 16.0), // 원하는 만큼 조절
+                    child: Icon(Icons.volume_up),
+                  ),
+                ],
+              ),
+              StepProgressBar(
+                theme == KioskTheme.fromMode(KioskMode.burger) ? '햄버거' : '커피',
+                theme == KioskTheme.fromMode(KioskMode.burger)
+                    ? Icons.lunch_dining
+                    : Icons.coffee,
+                theme == KioskTheme.fromMode(KioskMode.burger) ? '햄버거' : '커피',
+                theme == KioskTheme.fromMode(KioskMode.burger)
+                    ? Icons.lunch_dining
+                    : Icons.coffee,
+                theme == KioskTheme.fromMode(KioskMode.burger) ? '햄버거' : '커피',
+                theme == KioskTheme.fromMode(KioskMode.burger)
+                    ? Icons.lunch_dining
+                    : Icons.coffee,
+                theme == KioskTheme.fromMode(KioskMode.burger) ? '햄버거' : '커피',
+                theme == KioskTheme.fromMode(KioskMode.burger)
+                    ? Icons.lunch_dining
+                    : Icons.coffee,
+                theme,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
