91f552682acb7bcf460eb8c4456177d32ea01614
unknown <rlagustn070@naver.com>
2025-05-12 17:19:46 +0900

    feat : menu_state추가

--- lib/core/widgets/kiosk/step_progress_bar.dart
+++ lib/core/widgets/kiosk/step_progress_bar.dart
@@ -5,50 +5,30 @@ import 'package:team_kiosk/core/constants/theme_provider.dart';
 
 @immutable
 class StepProgressBar extends ConsumerWidget {
-  final String title;
-  final IconData icon;
-  final String title2;
-  final IconData icon2;
-  final String? title3;
-  final IconData? icon3;
-  final String? title4;
-  final IconData? icon4;
+  final void Function(int) onTap;
+  final List<String> titles;
+  final List<IconData> icons;
   final KioskTheme theme;
 
-  const StepProgressBar(
-    this.title,
-    this.icon,
-    this.title2,
-    this.icon2, {
-    this.title3,
-    this.icon3,
-    this.title4,
-    this.icon4,
+  const StepProgressBar({
+    required this.onTap,
+    required this.titles,
+    required this.icons,
     required this.theme,
     super.key,
-  });
+  }) : assert(
+         titles.length == icons.length && titles.length <= 4,
+         'titles and icons must have the same length and be at most 4 items long.',
+       );
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final styles = ref.watch(textStyleSetProvider);
-
-    final List<Tab> tabs = [
-      Tab(icon: Icon(icon), text: title),
-      Tab(icon: Icon(icon2), text: title2),
-    ];
-
-    if (title3 != null && icon3 != null) {
-      tabs.add(Tab(icon: Icon(icon3), text: title3));
-    }
-
-    if (title4 != null && icon4 != null) {
-      tabs.add(Tab(icon: Icon(icon4), text: title4));
-    }
-
     return Container(
       color: Colors.white,
       padding: const EdgeInsets.only(top: 8),
       child: TabBar(
+        onTap: onTap,
         labelColor: theme.primary,
         labelStyle: styles.button.copyWith(fontSize: 18),
         indicatorColor: theme.primary,
@@ -59,7 +39,10 @@ class StepProgressBar extends ConsumerWidget {
           color: Colors.grey,
           fontSize: 18,
         ),
-        tabs: tabs,
+        tabs: List.generate(
+          titles.length,
+          (index) => Tab(icon: Icon(icons[index]), text: titles[index]),
+        ),
       ),
     );
   }
