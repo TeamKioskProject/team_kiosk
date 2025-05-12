926f622437bdebb76785ea218ee58d99356d4c6c
unknown <rlagustn070@naver.com>
2025-05-12 13:14:02 +0900

    feat : dessert_mock_data.json 파일 받아오는 메서드 생성

---
+++ lib/data/data_source/hamburger_data_source_impl.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:team_kiosk/data/data_source/hamburger_data_source.dart';
+import 'package:team_kiosk/data/dto/hamburger_dto.dart';
+
+class HamburgerDataSourceImpl implements HamburgerDataSource {
+  static const String burgerPath = 'lib/data/data/burger_mock_data.json';
+  static const String dessertPath = 'lib/data/data/dessert_mock_data.json';
+
+  @override
+  Future<List<HamburgerDto>> getBurgerData() async {
+    try {
+      final jsonString = await rootBundle.loadString(burgerPath);
+      final Map<String, dynamic> jsonResponse = jsonDecode(jsonString);
+
+      final List<dynamic> jsonList = jsonResponse['burgers'];
+      final burgerList = jsonList.map((e) => HamburgerDto.fromJson(e)).toList();
+
+      return burgerList;
+    } catch (e) {
+      throw Exception('burger.json 로드 실패: $e');
+    }
+  }
+
+  @override
+  Future<List<HamburgerDto>> getDessertData() async {
+    try {
+      final jsonString = await rootBundle.loadString(dessertPath);
+      final Map<String, dynamic> jsonResponse = jsonDecode(jsonString);
+
+      final List<dynamic> jsonList = jsonResponse['dessert'];
+      final dessertList =
+          jsonList.map((e) => HamburgerDto.fromJson(e)).toList();
+
+      return dessertList;
+    } catch (e) {
+      throw Exception('dessert.json 로드 실패: $e');
+    }
+  }
+
+  @override
+  Future<List<HamburgerDto>> getDrinkData() {
+    // TODO: implement getDrinkData
+    throw UnimplementedError();
+  }
+}
