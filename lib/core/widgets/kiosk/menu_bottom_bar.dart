61dce92b48de05c449dcef7dd01ac27f64f0eadb
unknown <rlagustn070@naver.com>
2025-05-12 15:29:08 +0900

    ui : menuBottomNavigationBar UI 수정

--- lib/core/widgets/kiosk/menu_bottom_bar.dart
+++ lib/core/widgets/kiosk/menu_bottom_bar.dart
@@ -13,11 +13,18 @@ class MenuBottomBar extends ConsumerWidget {
     final styles = ref.watch(textStyleSetProvider);
     return Container(
       color: Colors.white,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text('선택된 메뉴', style: styles.caption),
-          const SizedBox(width: 10),
-          const Text('0'),
+          Row(
+            children: [
+              Text('선택된 메뉴', style: styles.caption),
+              const SizedBox(width: 15),
+              Text('0', style: styles.accent),
+            ],
+          ),
+
           ElevatedButton(
             onPressed: () {},
             child: Text('다음으로', style: styles.button),
