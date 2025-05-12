5c21e092e82872ab7daa720aad0f1ae8c6a8e537
YUNSEO <og737596@gmail.com>
2025-05-12 16:16:06 +0900

    feat : 데이터 초기화 세팅

--- lib/view/main_menu/menu_select_screen.dart
+++ lib/view/main_menu/menu_select_screen.dart
@@ -16,8 +16,8 @@ class MenuSelectScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(kioskThemeProvider);
-    final appState = ref.read(appStateProvider);
-    final style = ref.read(textStyleSetProvider);
+    final appState = ref.watch(appStateProvider);
+    final style = ref.watch(textStyleSetProvider);
     final state = ref.watch(menuSelectNotifierProvider);
     final viewModel = ref.watch(menuSelectNotifierProvider.notifier);
 
@@ -35,51 +35,50 @@ class MenuSelectScreen extends ConsumerWidget {
           ],
           bottom: PreferredSize(
             preferredSize: const Size.fromHeight(80),
-            child: appState.mode == AppMode.burger
-                ? StepProgressBar(
-              onTap: (index) {
-                viewModel.changeMode(index: index);
-              },
-              titles: ['햄버거', '사이드', '음료', '디저트'],
-              icons: List.generate(4, (index) => Icons.lunch_dining),
-              theme: theme,
-            )
-                : StepProgressBar(
-              onTap: (index) {
-                viewModel.changeMode(index: index);
-              },
-              titles: ['음료', '디저트'],
-              icons: List.generate(2, (index) => Icons.lunch_dining),
-              theme: theme,
-            ),
+            child:
+                appState.mode == AppMode.burger
+                    ? StepProgressBar(
+                      onTap: (int) {
+                        viewModel.changeMode(
+                          appStateMode: appState,
+                          index: int,
+                        );
+                      },
+                      titles: ['햄버거', '사이드', '음료', '디저트'],
+                      icons: List.generate(4, (index) => Icons.lunch_dining),
+                      theme: theme,
+                    )
+                    : StepProgressBar(
+                      onTap: (int) {
+                        viewModel.changeMode(
+                          appStateMode: appState,
+                          index: int,
+                        );
+                      },
+                      titles: ['음료', '디저트'],
+                      icons: List.generate(2, (index) => Icons.lunch_dining),
+                      theme: theme,
+                    ),
           ),
         ),
         body: Container(
           color: theme.background,
-          child: state.when(
-            data: (menuState) => appState.mode == AppMode.burger
-                ? TabBarView(
-              children: [
-                _buildMenuGrid(menuState.itemList, theme),
-                _buildMenuGrid(menuState.itemList, theme),
-                _buildMenuGrid(menuState.itemList, theme),
-                _buildMenuGrid(menuState.itemList, theme),
-              ],
-            )
-                : TabBarView(
-              children: [
-                _buildMenuGrid(menuState.itemList, theme),
-                _buildMenuGrid(menuState.itemList, theme),
-              ],
-            ),
-            loading: () => const Center(child: CircularProgressIndicator()),
-            error: (error, stack) => Center(
-              child: Text(
-                '오류 발생: $error',
-                style: style.body,
-              ),
-            ),
-          ),
+          child:
+              appState.mode == AppMode.burger
+                  ? TabBarView(
+                    children: [
+                      _buildMenuGrid(state.itemList, theme),
+                      _buildMenuGrid(state.itemList, theme),
+                      _buildMenuGrid(state.itemList, theme),
+                      _buildMenuGrid(state.itemList, theme),
+                    ],
+                  )
+                  : TabBarView(
+                    children: [
+                      _buildMenuGrid(state.itemList, theme),
+                      _buildMenuGrid(state.itemList, theme),
+                    ],
+                  ),
         ),
       ),
     );
