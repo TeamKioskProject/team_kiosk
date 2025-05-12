100d11f89846b0082989d8bb237a472f0974f1b5
unknown <rlagustn070@naver.com>
2025-05-12 13:30:56 +0900

    feat : burger_provider에 디저트와 음료수 provider 추가

--- lib/view/main_select/burger_provider/burger_provider.dart
+++ lib/view/main_select/burger_provider/burger_provider.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:team_kiosk/data/data_source/burger_data_source_impl.dart';
+import 'package:team_kiosk/data/data_source/hamburger_data_source_impl.dart';
 import 'package:team_kiosk/data/model/burger.dart';
-import 'package:team_kiosk/data/repository/burger_repository.dart';
-import 'package:team_kiosk/domain/repository/burger_repository_impl.dart';
+import 'package:team_kiosk/data/repository/hamburger_repository.dart';
+import 'package:team_kiosk/domain/repository/hamburger_repository_impl.dart';
 
+//햄버거 리스트 Provider
 final burgerListProvider = FutureProvider.autoDispose<List<Burger>>((
   ref,
 ) async {
@@ -11,6 +12,20 @@ final burgerListProvider = FutureProvider.autoDispose<List<Burger>>((
   return await repository.getBurgers();
 });
 
-final burgerRepositoryProvider = Provider<BurgerRepository>((ref) {
-  return BurgerRepositoryImpl(dataSource: BurgerDataSourceImpl());
+// 디저트 리스트 Provider
+final dessertListProvider = FutureProvider.autoDispose<List<Burger>>((
+  ref,
+) async {
+  final repository = ref.watch(burgerRepositoryProvider);
+  return await repository.getDesserts();
+});
+
+// 음료 리스트 Provider
+final drinkListProvider = FutureProvider.autoDispose<List<Burger>>((ref) async {
+  final repository = ref.watch(burgerRepositoryProvider);
+  return await repository.getDrinks();
+});
+
+final burgerRepositoryProvider = Provider<HamburgerRepository>((ref) {
+  return HamburgerRepositoryImpl(dataSource: HamburgerDataSourceImpl());
 });
