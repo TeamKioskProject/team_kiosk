921fd5f867cf203198ea34f03e12e2d60e6de61d
unknown <rlagustn070@naver.com>
2025-05-12 17:09:53 +0900

    Feat : order_repository_impl 추가및 provider설정ㅇ

--- lib/data/mapper/order_item_mapper.dart
+++ lib/data/mapper/order_item_mapper.dart
@@ -1,10 +1,10 @@
 import 'package:team_kiosk/core/constants/app_colors.dart';
 import 'package:team_kiosk/data/data_source/data_type.dart';
-import 'package:team_kiosk/data/dto/buger_type_dto.dart';
+import 'package:team_kiosk/data/dto/burger_type_dto.dart';
 import 'package:team_kiosk/data/dto/cafe_type_dto.dart';
 import 'package:team_kiosk/data/model/order_item.dart';
 
-extension BurgerOrderItemMapper on BugerTypeDto {
+extension BurgerOrderItemMapper on BurgerTypeDto {
   OrderItem toOrderItemBurger({required CategoryType type}) {
     return OrderItem(
       theme: KioskMode.burger,
