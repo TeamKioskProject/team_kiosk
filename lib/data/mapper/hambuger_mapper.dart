6f100dbfad19cf073aa74f4ac897f690b749e4e6
unknown <rlagustn070@naver.com>
2025-05-12 13:20:11 +0900

    refactor : toBurger -> toHamburger로 변경

--- lib/data/mapper/hambuger_mapper.dart
+++ lib/data/mapper/hambuger_mapper.dart
@@ -2,7 +2,7 @@ import 'package:team_kiosk/data/dto/hamburger_dto.dart';
 import 'package:team_kiosk/data/model/burger.dart';
 
 extension HambugerMapper on HamburgerDto {
-  Burger toBurger() {
+  Burger toHamBurger() {
     return Burger(
       name: menuComponents ?? '',
       imageUrl: menuImgPath ?? '',
--- lib/domain/repository/hamburger_repository_impl.dart
+++ lib/domain/repository/hamburger_repository_impl.dart
@@ -1,18 +1,30 @@
 import 'package:team_kiosk/data/data_source/burger_data_source.dart';
-import 'package:team_kiosk/data/mapper/buger_mapper.dart';
+import 'package:team_kiosk/data/data_source/hamburger_data_source.dart';
 import 'package:team_kiosk/data/model/burger.dart';
-import 'package:team_kiosk/data/repository/burger_repository.dart';
+import 'package:team_kiosk/data/repository/hamburger_repository.dart';
 
-class HamburgerRepositoryImpl implements BurgerRepository {
-  final BurgerDataSource _dataSource;
+class HamburgerRepositoryImpl implements HamburgerRepository {
+  final HamburgerDataSource _dataSource;
 
-  BurgerRepositoryImpl({required BurgerDataSource dataSource})
-      : _dataSource = dataSource;
+  HamburgerRepositoryImpl({required HamburgerDataSource dataSource})
+    : _dataSource = dataSource;
 
   @override
   Future<List<Burger>> getBurgers() async {
     final dtoList = await _dataSource.getBurgerData();
     final burgerList = dtoList.map((e) => e.toBurger()).toList();
     return burgerList;
+  }
+
+  @override
+  Future<List<Burger>> getDesserts() {
+    // TODO: implement getDesserts
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Burger>> getDrinks() {
+    // TODO: implement getDrinks
+    throw UnimplementedError();
   }
 }
