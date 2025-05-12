5d00efa9c371814f5aa93d421e4ff69cce6aa98a
unknown <rlagustn070@naver.com>
2025-05-12 13:10:44 +0900

    feat : 햄버거 디저트 다루는 data_source 추상메서드 작성

--- lib/data/data_source/hamburger_data_source.dart
+++ lib/data/data_source/hamburger_data_source.dart
@@ -2,4 +2,6 @@ import 'package:team_kiosk/data/dto/hamburger_dto.dart';
 
 abstract interface class HamburgerDataSource {
   Future<List<HamburgerDto>> getBurgerData();
+
+  Future<List<HamburgerDto>> getDessertData();
 }
