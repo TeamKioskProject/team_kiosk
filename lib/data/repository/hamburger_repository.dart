82d0dac2410abe896e9e970214e8fc80d2ea41f0
unknown <rlagustn070@naver.com>
2025-05-12 13:17:45 +0900

    feat : repository에 디저트와 음료수 추가

---
+++ lib/data/repository/hamburger_repository.dart
@@ -0,0 +1,9 @@
+import 'package:team_kiosk/data/model/burger.dart';
+
+abstract interface class HamburgerRepository {
+  Future<List<Burger>> getBurgers();
+
+  Future<List<Burger>> getDesserts();
+
+  Future<List<Burger>> getDrinks();
+}
