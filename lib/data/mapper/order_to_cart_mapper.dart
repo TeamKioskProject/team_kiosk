c08ae0bfacea9ec643948ed838b7fca499d38c77
unknown <rlagustn070@naver.com>
2025-05-13 11:27:17 +0900

    feat : menucard 클릭시 장바구니와 연동

--- lib/data/mapper/order_to_cart_mapper.dart
+++ lib/data/mapper/order_to_cart_mapper.dart
@@ -1,9 +1,8 @@
-import 'package:team_kiosk/data/data_source/data_type.dart';
 import 'package:team_kiosk/data/model/order_item.dart';
 import 'package:team_kiosk/view/cart/cart_item.dart';
 
 extension OrderToCartMapper on OrderItem {
-  CartItem toCart({required CategoryType type}) {
+  CartItem toCart() {
     return CartItem(
       id: id,
       name: name,
--- lib/view/cart/cart_state.dart
+++ lib/view/cart/cart_state.dart
@@ -1,7 +1,7 @@
 // lib/features/cart/model/cart_state.dart
 
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:team_kiosk/core/widgets/payment/mock_data.dart';
+
 import 'cart_item.dart';
 
 part 'cart_state.freezed.dart';
@@ -11,21 +11,10 @@ abstract class CartState with _$CartState {
   const factory CartState({
     required List<CartItem> cartItems,
     required int totalAmount,
+    required int totalQuantity,
   }) = _CartState;
 
-  factory CartState.initial() => CartState(
-    cartItems:
-        mockData
-            .map(
-              (items) => CartItem(
-                id: items["menuCd"],
-                name: items["menuNm"],
-                quantity: 1,
-                price: items["price"],
-                imagePath: items["menuImgThumPath"],
-              ),
-            )
-            .toList(),
-    totalAmount: mockData.fold<int>(0, (sum, items) => sum + (items["price"] * 1 as int)),
-  );
+  factory CartState.initial() {
+    return const CartState(cartItems: [], totalAmount: 0, totalQuantity: 0);
+  }
 }
